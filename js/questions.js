5a4a3f9df2b6e0c4e2be9022c24da59246cca622
Youssef Osama <bshgsgstrrt55@gmail.com>
2022-03-30 16:06:04 +0300

    Added the files

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "sin360?",
+    answer: "0",
+    options: [
+      "10",
+      "0",
+      "1",
+      "None"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ما هي الكتلة المولية؟ ",
+    answer: "هي الكتلة بالغرام لمول واحد",
+    options: [
+      "هي الكتلة بالغرام لمول واحد",
+      "هي كتلة ذرة واحدة من العنصر",
+      "هي كتلة العنصر التي تتحد او تزيح ثمان اجزاء كتلة الاوكسجين",
+      "كل ما تم ذكره بالاعلى"
+    ]
+  },
+    {
+    numb: 3,
+    question: "اذ كان المول واحد في الظروف القياسية فأن الحجم يساوي",
+    answer: "22.4",
+    options: [
+      "13",
+      "167",
+      "22.4",
+      "9.23"
+    ]
+  },
+    {
+    numb: 4,
+    question: "5π/6",
+    answer: "150",
+    options: [
+      "300",
+      "180",
+      "220",
+      "150"
+    ]
+  },
+    {
+    numb: 5,
+    question: " عداد  قيمته الاولية 2 و النقصان بمقدار 2، ما قيمته النهائية بعد 3 دورات",
+    answer: "-4",
+    options: [
+      "-4",
+      "4",
+      "3",
+      "-6"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
